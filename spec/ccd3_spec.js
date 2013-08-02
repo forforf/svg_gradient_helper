921b3103bac5bfde82fc367b591189b35ec1c97c
Dave <dmarti21@gmail.com>
2013-08-02 15:55:15 -0400

    initial boiler plate

---
+++ spec/ccd3_spec.js
@@ -0,0 +1,7 @@
+describe('ccd3', function() {
+    describe('linearGradientBox', function() {
+        it('passes sanity checks', function(){
+            expect(ccd3.linearGradientBox).toBeDefined();
+        })
+    })
+});
---
+++ src/ccd3.js
@@ -0,0 +1,13 @@
+window.ccd3 = (function() {
+  function Ccd3(els) {
+
+  }
+
+  //main object
+  var ccd3 = {
+    linearGradientBox: "foo"
+  };
+
+
+  return ccd3;
+})();
